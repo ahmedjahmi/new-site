9d9447bdb78f804e098e51788889848765dee7c2
ahmedJahmi <amjahmi@gmail.com>
2021-02-20 20:34:51 -0500

    Edit home bio

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,12 @@ export default function Home() {
 			</Head>
 			<section className={utilStyles.headingMd}>
 				<p>
-					Allow me to reintroduce myself. My name is Ho- ...just kidding. My
-					name is Ahmed Jahmi.
+					My name is Ahmed Jahmi. I'm a Full Stack Developer that thinks beyond
+					the current problem, considering the larger context and seeking out
+					the bigger picture. Adaptive to change and willing to learn, I've had
+					the opportunity to work with many different languages and frameworks
+					including JavaScript, React, Python, and Django. Every problem is
+					unique, and so should be their solutions.
 				</p>
 			</section>
 		</Layout>
