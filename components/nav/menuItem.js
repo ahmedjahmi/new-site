cd8cb73cc34b8ae938dcb2250d849ecb898d894d
ahmedJahmi <amjahmi@gmail.com>
2021-03-12 20:32:25 -0500

    Add new nav

--- components/nav/menuItem.js
+++ components/nav/menuItem.js
@@ -3,5 +3,22 @@ import styles from './nav.module.scss';
 import { useState } from 'react';
 
 export default function MenuItem(props) {
-  
+	const [open, setOpen] = useState(props.open);
+
+	// conditionally set styles
+	const [delay, setDelay] = useState(`${props.delay}`);
+	const toggle = open
+		? `${styles.menuItemContainer} ${styles.menuItemContainerOpen}`
+		: `${styles.menuItemContainer}`;
+
+	return (
+		<div className={toggle} delay={delay}>
+			<div className={styles.menuItem} onClick={props.onClick} delay={delay}>
+				<Link href={props.href}>
+					<a>{props.children}</a>
+				</Link>
+			</div>
+			<div className={styles.menuItemLine} delay={delay} />
+		</div>
+	);
 }
--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,88 +1,52 @@
 import Link from 'next/link';
 import styles from './nav.module.scss';
-import {useState} from 'react';
-
-const name = 'Ahmed Jahmi';
-
-// export default function Nav() {
-//   return (
-// 		<nav className={styles.nav}>
-// 			<div className={styles.navImage}>
-// 				<Link href='/'>
-// 					<a>
-// 						<img
-// 							src='/images/profile.jpg'
-// 							alt={name}
-// 						/>
-// 					</a>
-// 				</Link>
-// 			</div>
-// 			<ul>
-// 				<li>
-// 					<Link href='/blog'>
-// 						<a>blog</a>
-// 					</Link>
-// 				</li>
-// 				<li>
-// 					<Link href='/projects'>
-// 						<a>projects</a>
-// 					</Link>
-// 				</li>
-// 				<li>
-// 					<Link href='/contact'>
-// 						<a>contact</a>
-// 					</Link>
-// 				</li>
-// 				<li>
-//           <Link href='/about'>
-//             <a>about</a>
-//           </Link>
-//         </li>
-// 			</ul>
-//       <div className={styles.mobileBar}>
-//         <span></span>
-//         <span></span>
-//         <span></span>
-//       </div>
-// 		</nav>
-// 	);
-// }
+import { useState, useEffect } from 'react';
+import MenuItem from './menuItem';
+import MenuButton from './menuButton';
+import Menu from './menu';
 
 export default function Nav() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const handleMenuClick = () => {
-    setMenuOpen(!menuOpen);
-  }
-  const handleLinkClick = () => {
-    setMenuOpen(false);
-  }
+	const [menuOpen, setMenuOpen] = useState(false);
 
-  const menu = ['home', 'blog', 'projects', 'contact']
+	const handleMenuClick = () => {
+		setMenuOpen(!menuOpen);
+	};
+	const handleLinkClick = () => {
+		setMenuOpen(false);
+	};
 
-  const menuItems = menu.map((val, index) => {
-    const pathFromVal = val !== 'home' ? `/${val}` : '/';
-    const newVal = val.charAt(0).toUpperCase() + val.slice(1);
-    return (
-      <MenuItem
-        key={index}
-        // delay={`${index * 0.1}s`}
-        onClick={handleLinkClick}
-      >
-        <Link href={pathFromVal}>
-          <a>{newVal}</a>
-        </Link>
-      </MenuItem>
-    )
-  })
-  return (
-    <>
-      <div className={styles.nav}>
-        <MenuButton open={menuOpen} onClick={handleMenuClick} />
-        <div className={styles.logo}>Logo</div>
-      </div>
-      <Menu open={menuOpen}>
-        {menuItems}
-      </Menu>
-    </>
-  )
+	const menu = ['home', 'blog', 'projects', 'contact'];
+
+	const menuItems = menu.map((val, index) => {
+		const pathFromVal = val !== 'home' ? `/${val}` : '/';
+		const newVal = val.charAt(0).toUpperCase() + val.slice(1);
+		return (
+			<MenuItem
+				key={index}
+				delay={index}
+				onClick={handleLinkClick}
+				href={pathFromVal}
+				open={menuOpen}
+			>
+				{newVal}
+			</MenuItem>
+		);
+	});
+
+	return (
+		<>
+			<div className={styles.nav}>
+				<MenuButton open={menuOpen} onClick={handleMenuClick} />
+				<div className={styles.avatar}>
+					<img
+						src='/images/profile.jpg'
+						alt='ahmed jahmi'
+						className={styles.avatarImage}
+					/>
+				</div>
+				<div />
+			</div>
+			<Menu open={menuOpen}>{menuItems}</Menu>
+		</>
+	);
 }
