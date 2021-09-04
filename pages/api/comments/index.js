07650a0f59bf91c8a3a25c0ed5bac75bbbb4a1ae
ahmedJahmi <amjahmi@gmail.com>
2021-09-04 07:50:31 -0400

    Edit comment controller and api route

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -3,7 +3,7 @@ import createComment from '../../../lib/controllers/createComment';
 export default async function handler(req, res) {
 	switch (req.method) {
 		case 'POST':
-			return createComment({ req: req, res: res });
+			return createComment(req, res);
 		// TODO:
 		// case 'GET':
 		//   return getComments(req, res);
