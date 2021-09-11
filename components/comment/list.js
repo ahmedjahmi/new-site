4ae5f461978dfea025eb1acbbed566098dcf8f2d
ahmedJahmi <amjahmi@gmail.com>
2021-09-11 08:41:24 -0400

    Add styles to comments list

--- components/comment/list.js
+++ components/comment/list.js
@@ -1,4 +1,5 @@
 import distanceToNow from '../../lib/dateRelative';
+import styles from './comment.module.scss';
 
 function CommentList({
 	comments,
@@ -13,30 +14,43 @@ function CommentList({
 	if (isError) return <div>{swrError}</div>;
 	return (
 		<div>
-			{comments.map((comment) => {
-				const isAuthor = dbUser && dbUser._id === comment.user._id;
+			<div className={styles.CommentsHeader}>
+				<h3 className={styles.CommentsHeading}>
+					<span>Comments</span>
+				</h3>
+			</div>
+			<div className={styles.CommentList}>
+				{comments.map((comment) => {
+					const isAuthor = dbUser && dbUser._id === comment.user._id;
 
-				return (
-					<div key={comment.createdAt}>
-						{/* TODO: */}
-						{/* <div>
-              <img />
-            </div> */}
-
-						<div>
-							<div>
-								<b>{comment.user.username}</b>
-								<time>{distanceToNow(comment.createdAt)}</time>
+					return (
+						<div key={comment.createdAt} className={styles.Comment}>
+							<div className={styles.CommentHeader}>
+								<div className={styles.CommentImage}>
+									<img src='https://via.placeholder.com/40' />
+								</div>
+								<div className={styles.CommentInfo}>
+									<div className={styles.CommentUser}>
+										<b>{comment.user.username}</b>
+									</div>
+									<div className={styles.CommentDate}>
+										<time>{distanceToNow(comment.createdAt)}</time>
+									</div>
+								</div>
 								{(isAdmin || isAuthor) && (
-									<button onClick={() => onDelete(comment._id)}>X</button>
+									<div className={styles.CommentDelete}>
+										<button onClick={() => onDelete(comment._id)}>X</button>
+									</div>
 								)}
 							</div>
 
-							<div>{comment.text}</div>
+							<div className={styles.CommentBody}>
+								<div className={styles.CommentText}>{comment.text}</div>
+							</div>
 						</div>
-					</div>
-				);
-			})}
+					);
+				})}
+			</div>
 		</div>
 	);
 }
