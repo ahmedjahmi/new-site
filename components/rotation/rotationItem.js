230415e6af31263d65590352b7e12db43ce3fd7c
ahmedJahmi <amjahmi@gmail.com>
2021-09-29 15:59:59 -0400

    Remove unused prop

--- components/rotation/rotationItem.js
+++ components/rotation/rotationItem.js
@@ -2,7 +2,7 @@ import Card from '../card';
 import styles from './rotation.module.scss';
 import Link from 'next/link';
 
-export default function RotationItem({ key, rotationType, url, title }) {
+export default function RotationItem({ rotationType, url, title }) {
 	return (
 		<Link href={url}>
 			<a href={url} target='_blank'>
