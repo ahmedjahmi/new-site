bc76fd4eec3efbd573dca7b10e9e912069990169
ahmedJahmi <amjahmi@gmail.com>
2021-09-29 15:57:42 -0400

    Add flex row modifier to card

--- components/card/index.js
+++ components/card/index.js
@@ -1,9 +1,17 @@
 import styles from './card.module.scss';
 
-function Card({ children, key }) {
+function Card({ children, key, modifier }) {
+	const isHorizontal = modifier === 'horizontal' ? true : false;
 	return (
 		<>
-			<div key={key} className={styles.card}>
+			<div
+				key={key}
+				className={
+					isHorizontal
+						? `${styles.card} ${styles.cardHorizontal}`
+						: `${styles.card}`
+				}
+			>
 				{children}
 			</div>
 		</>
