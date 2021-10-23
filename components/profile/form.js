f20c10f27878673ae997a778de5046a68d39224d
ahmedJahmi <amjahmi@gmail.com>
2021-10-22 20:27:40 -0400

    Remove import

--- components/profile/form.js
+++ components/profile/form.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import styles from './profile.module.scss';
 import axios from 'axios';
 
 function ProfileForm({ profileForm, userId, formId }) {
